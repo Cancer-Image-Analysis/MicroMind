446c02f813a5d5d852f17f1534dacbb0f78da2eb
kevin <kevin.cortacero@inserm.fr>
2021-01-26 17:25:15 +0100

    many mask functions

--- cania_utils/image.py
+++ cania_utils/image.py
@@ -2,7 +2,9 @@ import tifffile
 import cv2
 import numpy as np
 
-from cania_utils.geometry2D.vector2D import Vector
+from cania_utils.vector2D import Vector
+
+BINARY_FILL_COLOR = 255
 
 """ read images """
 
@@ -94,7 +96,7 @@ def overlay(image, mask, color=[255, 255, 0], alpha=0.4, border_color='same'):
 
 def fill_ellipses(mask, ellipses):
     for ellipse in ellipses:
-        cv2.ellipse(mask, ellipse, 1, thickness=-1)
+        cv2.ellipse(mask, ellipse, BINARY_FILL_COLOR, thickness=-1)
     return mask
 
 
@@ -110,13 +112,23 @@ def count_in_mask(image, mask, threshold=0):
     return np.count_nonzero(cv2.bitwise_and(image_th, image_th, mask=mask))
 
 
+def max_in_mask(image, mask):
+    image_on_mask = cv2.bitwise_and(image, image, mask=mask)
+    only_positive_values = image_on_mask[np.argwhere(image_on_mask)]
+    if len(only_positive_values) == 0:
+        return 0.
+    return np.max(only_positive_values)
+
+
 def mean_in_mask(image, mask):
-    return np.mean(cv2.bitwise_and(image, image, mask=mask))
+    image_on_mask = cv2.bitwise_and(image, image, mask=mask)
+    only_positive_values = image_on_mask[np.argwhere(image_on_mask)]
+    return np.mean(only_positive_values)
 
 
 def split_mask_with_line(mask, line):
     line_mask = imnew(mask.shape)
-    line_mask = cv2.line(line_mask, line[0], line[1], 1, 2)
+    line_mask = cv2.line(line_mask, line[0], line[1], BINARY_FILL_COLOR, 2)
     splitted_mask = cv2.bitwise_and(mask, cv2.bitwise_not(line_mask))
     contours, _ = cv2.findContours(splitted_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     submasks = []
@@ -134,13 +146,25 @@ def split_mask_with_line(mask, line):
 
 def intersection_with_line(mask, line):
     line_mask = imnew(mask.shape)
-    line_mask = cv2.line(line_mask, line[0], line[1], 1, 2)
+    line_mask = cv2.line(line_mask, line[0], line[1], BINARY_FILL_COLOR, 2)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     mask_cnt = imnew(mask.shape)
-    cv2.drawContours(mask_cnt, contours, -1, 1, 2)
+    cv2.drawContours(mask_cnt, contours, -1, BINARY_FILL_COLOR, 2)
     intersection = cv2.bitwise_and(line_mask, mask_cnt)
     centroid = np.mean(np.argwhere(intersection), axis=0)
     return centroid
+
+
+def mean_over_line(image, line):
+    line_mask = imnew(image.shape)
+    line_mask = cv2.line(line_mask, line[0], line[1], BINARY_FILL_COLOR, 2)
+    return mean_in_mask(image, line_mask)
+
+
+def max_over_line(image, line):
+    line_mask = imnew(image.shape)
+    line_mask = cv2.line(line_mask, line[0], line[1], BINARY_FILL_COLOR, 2)
+    return max_in_mask(image, line_mask)
 
 
 def extract_rectangle_area(image, center, theta, width, height):
@@ -172,7 +196,7 @@ def imfill(image):
     mask = imnew((h+2, w+2))
 
     # Floodfill from point (0, 0)
-    cv2.floodFill(im_floodfill, mask, (0, 0), 255)
+    cv2.floodFill(im_floodfill, mask, (0, 0), BINARY_FILL_COLOR)
 
     # Invert floodfilled image
     im_floodfill_inv = cv2.bitwise_not(im_floodfill)
