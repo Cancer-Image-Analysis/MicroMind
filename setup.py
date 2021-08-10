7ef89c571a96e641b94aeb9c7ea6a9efeb8e63dd
Kévin Cortacero <kevin.cortacero@inserm.fr>
2021-08-10 18:24:52 +0200

    update cania-utils package to MicroMind package

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
 from setuptools import setup
 
-
-setup(name='cania-utils',
-      version='1.0',
-      description='All utils for Cancer Image Analysis python package',
-      long_description='',
-      classifiers=[
-        'Development Status :: 1 - Planning',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Intended Audience :: Healthcare Industry',
-      ],
-      keywords='cancer artificial-intelligence computer-vision',
-      url='https://github.com/Cancer-Image-Analysis/cania-utils',
-      author='Kevin Cortacero',
-      author_email='kevin.cortacero@inserm.fr',
-      license='MIT',
-      packages=['cania_utils'],
-      install_requires=[
-          'numpy',
-          'tifffile',
-          'pandas',
-          'opencv-python',
-          'czifile',
-          'scikit-image',
-          'scikit-learn',
-      ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      include_package_data=True,
-      zip_safe=False)
+setup()
--- src/micromind/io/drive/disk.py
+++ src/micromind/io/drive/disk.py
@@ -2,7 +2,8 @@ from pathlib import Path
 import zipfile
 import pandas as pd
 
-from cania_utils.image import imread_color, imread_tiff, imwrite, imread_czi, imwrite_tiff
+from micromind.io.image import (imread_color, imread_tiff, imread_czi,
+                                imwrite, imwrite_tiff)
 
 PNG = '.png'
 JPG = '.jpg'
@@ -13,14 +14,21 @@ LSM = '.lsm'
 CZI = '.czi'
 
 
-class Disk(object):
-    def __init__(self, location):
-        self.location = Path(location)
-        if not self.location.exists():
-            raise FileNotFoundError()
+class DriveObject(Path):
+    def __init__(self, path):
+        super().__init__(path)
+
+
+class Directory(DriveObject):
+    def __init__(self, path):
+        super().__init__(path)
+        if not self.exists():
+            raise ValueError(f'The directory {self} does not exist!')
+        if not self.is_dir():
+            raise ValueError(f'The path {self} is not pointing to a directory!')
 
     def write(self, filename, filedata):
-        filepath = self.location / filename
+        filepath = self / filename
         extension = filepath.suffix
         filepath = str(filepath)
         if extension == PNG:
@@ -29,7 +37,7 @@ class Disk(object):
             imwrite_tiff(filepath, filedata)
 
     def read(self, filename):
-        filepath = self.location / filename
+        filepath = self / filename
         extension = filepath.suffix
         filepath = str(filepath)
         if extension == PNG or extension == JPG:
@@ -42,31 +50,40 @@ class Disk(object):
             return imread_czi(filepath)
 
     def unzip(self, filename):
-        filepath = self.location / filename
+        filepath = self / filename
         extension = filepath.suffix
         filepath = str(filepath)
         unzip_folder = filename.replace(ZIP, '')
-        new_location = self.location / unzip_folder
+        new_location = self / unzip_folder
         if new_location.exists():
-            return Disk(new_location)
+            return Directory(new_location)
 
         if extension == ZIP:
             with zipfile.ZipFile(filepath, 'r') as zip_ref:
                 zip_ref.extractall(str(new_location))
-            return Disk(new_location)
+            return Directory(new_location)
 
     def next(self, next_location):
-        filepath = self.location / next_location
+        filepath = self / next_location
         filepath.mkdir(parents=True, exist_ok=True)
-        return Disk(filepath)
+        return Directory(filepath)
 
     def ls(self, regex='*'):
-        return self.location.glob(regex)
+        return self.glob(regex)
 
     def save_as_csv(self, data, filename):
-        filepath = self.location / filename
+        filepath = self / filename
         if isinstance(data, dict):
             df = pd.DataFrame(data=data)
         elif isinstance(data, pd.DataFrame):
             df = data
         df.to_csv(str(filepath))
+
+
+class File(DriveObject):
+    def __init__(self, path):
+        super().__init__(path)
+        if not self.exists():
+            raise ValueError(f'The file {self} does not exist!')
+        if not self.is_dir():
+            raise ValueError(f'The path {self} is not pointing to a file!')
