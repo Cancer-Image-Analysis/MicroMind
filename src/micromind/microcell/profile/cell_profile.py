485f80be72eba5d559074292a06bfce36167e73e
Kévin Cortacero <kevin.cortacero@inserm.fr>
2022-03-28 19:23:26 +0200

    better cell profiling

--- src/micromind/microcell/profile/cell_profile.py
+++ src/micromind/microcell/profile/cell_profile.py
@@ -4,46 +4,45 @@ import numpy as np
 
 
 class CellStainingProfile:
-    def __init__(self, channels, names, threshold=0, sep=" "):
-        self.channels = channels
-        self.names = names
-        self.threshold = threshold
-        self.sep = sep
-        self.lst = list(itertools.product([0, 1], repeat=len(channels)))
+    def __init__(self, infos):
+        self.infos = infos
+        self.lst = list(itertools.product([0, 1], repeat=len(infos.channels)))
         self.lst = sorted(self.lst, key=lambda c: sum(c), reverse=True)
 
     def get_profile(self, cell, stainings):
         inside_cell = cell.mask > 0
         labels = []
         profile = []
-        channels = {
-            name: stainings[:, channel]
-            for channel, name in zip(self.channels, self.names)
-        }
         channels_true = {}
         channels_false = {}
-        for name, channel in channels.items():
+        for i, channel in self.infos.channels.items():
+            channel_data = stainings[:, i]
+            name = channel["name"]
+            threshold = channel["threshold"]
             z_stack = [
-                np.logical_and.reduce([z > self.threshold, inside_cell])
-                for z in channel
+                np.logical_and.reduce([z >= threshold[0], inside_cell])
+                for z in channel_data
             ]
             channels_true[name] = z_stack
             channels_false[name] = np.bitwise_not(z_stack)
 
         for combination in self.lst:
-            if combination == (0, 0, 0):
-                continue
             combination_names = []
             combination_values = []
-            for i in range(len(self.channels)):
-                name = self.names[i]
+            for i, channel in self.infos.channels.items():
+                name = channel["name"]
                 if combination[i] == 1:
                     combination_names.append(name)
                     combination_values.append(channels_true[name])
                 else:
                     combination_values.append(channels_false[name])
             s = np.count_nonzero(np.logical_and.reduce(combination_values))
-            labels.append(f" {self.sep} ".join(combination_names))
+            label_name = f"{self.infos.separator}".join(combination_names)
+            labels.append(label_name)
             profile.append(s)
 
+        for i, channel in self.infos.channels.items():
+            name = channel["name"]
+            labels.append(f"{name}+")
+            profile.append(np.count_nonzero(channels_true[name]))
         return profile, labels
