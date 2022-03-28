fa488c08646c938a6cdf48be089ce7ace9987f58
Kévin Cortacero <kevin.cortacero@inserm.fr>
2022-03-28 19:23:00 +0200

    better marker plot

--- src/micromind/plotting/plot.py
+++ src/micromind/plotting/plot.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 from matplotlib import rc
-
+import numpy as np
+import cv2
 from micromind.cv.image import contours, draw_contours
 
 rc("font", **{"size": 6})
 
 
-def plot_predictions(original, prediction, contours_color=[255, 255, 0]):
+def plot_predictions(original, prediction, contours_color=[255, 255, 0], only_centroid=False):
     fig = plt.figure(figsize=(6, 18), dpi=300)
 
     ax = fig.add_subplot(1, 3, 1)
@@ -21,11 +22,25 @@ def plot_predictions(original, prediction, contours_color=[255, 255, 0]):
 
     borders = original.copy()
     cnts = contours(prediction)
-    borders = draw_contours(borders, cnts, color=contours_color)
-    ax = fig.add_subplot(1, 3, 3)
-    ax.imshow(borders)
-    ax.set_title("contours")
-    ax.axis("off")
+    if only_centroid:
+        for cnt in cnts:
+            cnt_x = cnt[:, 0, 0]
+            cnt_y = cnt[:, 0, 1]
+            print(cnt_x)
+            centroid_x = cnt_x.mean()
+            print(centroid_x)
+            centroid_y = cnt_y.mean()
+            cv2.circle(borders, (int(centroid_x), int(centroid_y)), 10, contours_color, -1)
+        ax = fig.add_subplot(1, 3, 3)
+        ax.imshow(borders)
+        ax.set_title("markers")
+        ax.axis("off")
+    else:
+        borders = draw_contours(borders, cnts, color=contours_color)
+        ax = fig.add_subplot(1, 3, 3)
+        ax.imshow(borders)
+        ax.set_title("contours")
+        ax.axis("off")
 
 
 def plot_watershed(original, mask, markers, watershed):
